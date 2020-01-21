1e093f156f019a172ba174ff54e3acd5bf130ac2
jamesohmcts <james.odukoya@hmcts.net>
2020-01-21 23:30:51 +0000

    adding margin,padding and style to amount

--- lib/main.dart
+++ lib/main.dart
@@ -9,30 +9,25 @@ class MyApp extends StatelessWidget {
     // TODO: implement build
     return MaterialApp(
       title: 'Flutter',
+      debugShowCheckedModeBanner: false,
       home: MyHomePage(),
     );
   }
-
 }
 // This widget is the root of your application.
 
 class MyHomePage extends StatelessWidget {
-
   final List<Transaction> transactions = [
     Transaction(
         id: 'r1', title: 'New shoes', amount: 69.99, date: DateTime.now()),
     Transaction(
         id: 'r2', title: 'New Tennis', amount: 16.53, date: DateTime.now()),
-
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.yellow,
-        title: Text('Money App')
-      ),
+      appBar: AppBar(backgroundColor: Colors.yellow, title: Text('Money App')),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -45,14 +40,42 @@ class MyHomePage extends StatelessWidget {
               elevation: 5,
             ),
           ),
-          Column(children: transactions.map((tx) {
-            return Card(child: Text(tx.title),
+          Column(
+              children: transactions.map((tx) {
+            return Card(
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 15,
+                    ),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black, width: 2),
+                    ),
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      tx.amount.toString(),
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.amberAccent,
+
+                      ),
+                    ),
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Text(tx.title),
+                      Text(tx.date.toString()),
+                    ],
+                  )
+                ],
+              ),
             );
-          }).toList()
-          ),
+          }).toList()),
         ],
       ),
     );
   }
 }
-
