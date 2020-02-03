1b0c68ba969a80d0832f2f1f55d9813ff59eee1c
jamesohmcts <james.odukoya@hmcts.net>
2020-02-03 17:09:15 +0000

    Add new transaction to view non-scrollable

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import './transaction.dart';
+import 'package:money_tracker_app/Widgets/user_transactions.dart';
 
 void main() => runApp(MyApp());
 
@@ -17,19 +17,18 @@ class MyApp extends StatelessWidget {
 // This widget is the root of your application.
 
 class MyHomePage extends StatelessWidget {
-  final List<Transaction> transactions = [
-    Transaction(
-        id: 'r1', title: 'New shoes', amount: 69.99, date: DateTime.now()),
-    Transaction(
-        id: 'r2', title: 'New Tennis', amount: 16.53, date: DateTime.now()),
-  ];
 
+//  String titleInput;
+//  String amountInput;
+
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(backgroundColor: Colors.yellow, title: Text('Money App')),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        //mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Container(
@@ -40,40 +39,7 @@ class MyHomePage extends StatelessWidget {
               elevation: 5,
             ),
           ),
-          Column(
-              children: transactions.map((tx) {
-            return Card(
-              child: Row(
-                children: <Widget>[
-                  Container(
-                    margin: EdgeInsets.symmetric(
-                      vertical: 10,
-                      horizontal: 15,
-                    ),
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.black, width: 2),
-                    ),
-                    padding: EdgeInsets.all(10),
-                    child: Text(
-                      tx.amount.toString(),
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Colors.amberAccent,
-
-                      ),
-                    ),
-                  ),
-                  Column(
-                    children: <Widget>[
-                      Text(tx.title),
-                      Text(tx.date.toString()),
-                    ],
-                  )
-                ],
-              ),
-            );
-          }).toList()),
+          UserTransaction()
         ],
       ),
     );
