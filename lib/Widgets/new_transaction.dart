cd0edbeeb11d8456a3a76f1ccc1b3c1ce822046d
jamesohmcts <james.odukoya@hmcts.net>
2020-02-04 12:46:02 +0000

    fixed typing in title and amount issues by making transation a stateful widget also added a navigator to close transation on submit

--- lib/Widgets/new_transaction.dart
+++ lib/Widgets/new_transaction.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 
-class NewTransaction extends StatelessWidget {
+class NewTransaction extends StatefulWidget {
   final Function addTx;
+
+  NewTransaction(this.addTx); // constructor
+
+  @override
+  _NewTransactionState createState() => _NewTransactionState();
+  }
+
+class _NewTransactionState extends State<NewTransaction> {
   final titleController = TextEditingController();
   final amountController = TextEditingController();
 
-  NewTransaction(this.addTx); // constructor
 
   void submitData (){
     final enteredTitle = titleController.text;
@@ -16,8 +23,11 @@ class NewTransaction extends StatelessWidget {
       return; // if both or one field is empty a transaction will not be added to list
     }
 
-      addTx(enteredTitle,enteredAmount);
+      widget.addTx(enteredTitle,enteredAmount);
+
+    Navigator.of(context).pop();
   }
+
 
   @override
   Widget build(BuildContext context) {
